d9dd2344c202be2fb5f57f01a810e0631bfe157d
Benoit Suttor <bsuttor@imio.be>
2019-03-13 11:23:00 +0100

    Add --init option if devpi is not initialized

--- run.sh
+++ run.sh
@@ -7,14 +7,10 @@ if [ ! -f "$DEVPI_SERVERDIR/.serverversion" ]; then
     initialize=no
 fi
 
-/srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --request-timeout 30 --outside-url http://${HOSTNAME}
-
 if [[ "$initialize" = "no" ]]; then
-  /srv/devpi/bin/devpi use http://localhost:3141
-  /srv/devpi/bin/devpi use root/pypi
-  /srv/devpi/bin/devpi login root --password=''
-  /srv/devpi/bin/devpi user -m root password="${DEVPI_PASSWORD}"
-  /srv/devpi/bin/devpi index -y -c imio pypi_whitelist='*'
+    /srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --request-timeout 60 --outside-url http://${HOSTNAME} --init
+else
+    /srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --request-timeout 60 --outside-url http://${HOSTNAME}
 fi
 
 tail -f $DEVPI_SERVERDIR/.xproc/devpi-server/xprocess.log
