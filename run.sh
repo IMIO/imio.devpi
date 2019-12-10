802e0683843fa22d640ca61fdee6c069e01fb279
Benoit Suttor <bsuttor@imio.be>
2019-12-10 09:54:03 +0100

    Use devpi-init instead of deprecated --init option

--- run.sh
+++ run.sh
@@ -3,14 +3,14 @@ set -e
 set -x
 
 initialize=yes
-if [ ! -f "$DEVPI_SERVERDIR/.serverversion" ]; then
+if [ ! -f "$DEVPISERVER_SERVERDIR/.serverversion" ]; then
     initialize=no
 fi
 
 if [[ "$initialize" = "no" ]]; then
-    /srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --request-timeout 60 --outside-url http://${HOSTNAME} --init
-else
-    /srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --request-timeout 60 --outside-url http://${HOSTNAME}
+    /srv/devpi/bin/devpi-init
 fi
+    
+/srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --request-timeout 60 --outside-url http://${HOSTNAME}
 
-tail -f $DEVPI_SERVERDIR/.xproc/devpi-server/xprocess.log
+tail -f $DEVPISERVER_SERVERDIR/.xproc/devpi-server/xprocess.log
