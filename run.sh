d992867abf5398bab4b49be4f57debe7f1f9e554
Benoit Suttor <bsuttor@imio.be>
2019-12-10 10:39:38 +0100

    Add server options : --indexer-backend=null --threads=20
    Because of this issue https://github.com/scrapy/scrapy/issues/2115

--- run.sh
+++ run.sh
@@ -11,6 +11,6 @@ if [[ "$initialize" = "no" ]]; then
     /srv/devpi/bin/devpi-init
 fi
     
-/srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --request-timeout 60 --outside-url http://${HOSTNAME}
+/srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --request-timeout 60 --outside-url http://${HOSTNAME} --indexer-backend=null --threads=20
 
 tail -f $DEVPISERVER_SERVERDIR/.xproc/devpi-server/xprocess.log
