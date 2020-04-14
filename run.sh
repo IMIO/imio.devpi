c8c9dc2a0bf62f0baeab171a0d674d064b18db80
Benoit Suttor <bsuttor@imio.be>
2020-04-14 15:13:39 +0200

    Set default value on devpi-server start

--- run.sh
+++ run.sh
@@ -11,6 +11,6 @@ if [[ "$initialize" = "no" ]]; then
     /srv/devpi/bin/devpi-init
 fi
 
-/srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --request-timeout 60 --proxy-timeout 60 --outside-url "https://${HOSTNAME}" --threads=20
+/srv/devpi/bin/devpi-server --start --host 0.0.0.0 --port 3141 --outside-url "https://${HOSTNAME}"
 
 tail -f "$DEVPISERVER_SERVERDIR/.xproc/devpi-server/xprocess.log"
